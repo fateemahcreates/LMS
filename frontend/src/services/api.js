2b862713367e8c2f8160f4f7fc4965d3c8213be0
TEEEEMAH <fateemahcreates@gmail.com>
2026-07-16 15:00:49 -0400

    Use Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,21 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: import.meta.env.VITE_API_URL, //baseURL: "http://localhost:5000/api",
-});  
-
-// Attach JWT token to every request
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  baseURL: "https://lms-backend.onrender.com/api",
+});
 
 export default api;
