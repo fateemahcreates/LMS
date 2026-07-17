f8cd34715f9d40663c1d7b1a9e0d0418229f6fe3
TEEEEMAH <fateemahcreates@gmail.com>
2026-07-17 10:04:20 -0400

    Add JWT interceptor

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,18 @@ const api = axios.create({
   baseURL: "https://lms-backend-g9mp.onrender.com/api",
 });
 
+// Attach JWT token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default api;
