cf835c85daa9b4ebaa9488d087cd5081524baa5e
TEEEEMAH <fateemahcreates@gmail.com>
2026-07-17 09:34:18 -0400

    Use Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lms-backend.onrender.com/api",
+  baseURL: "https://lms-backend-g9mp.onrender.com",
 });
 
 export default api;
